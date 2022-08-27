66a60eae02a6f530bc276501062cbb90edb1c1ee
troyleong7 <80636038+troyleong7@users.noreply.github.com>
2022-08-28 02:42:17 +1000

    Implementing the basic structure of Hamburger

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,20 +4,27 @@ import "./nav-style.css"
 function Navbar() {
     return (
         <nav className="nav">
-            <a href="/" className="home-title">
+            <div className="home-title">
                 UniRec
+            </div>
+            <a href="/#" className='toggle'>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
             </a>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/Buildings">Buildings</a>
-                </li>
-                <li>
-                    <a href="/Profile">Profile</a>
-                </li>
-            </ul>
+            <div className='bar-links'>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/Buildings">Buildings</a>
+                    </li>
+                    <li>
+                        <a href="/Profile">Profile</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
