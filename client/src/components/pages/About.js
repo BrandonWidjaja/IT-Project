458a960a7821a23aa4e32896eae95adfb56401dd
troyleong7 <80636038+troyleong7@users.noreply.github.com>
2022-09-03 19:03:44 +1000

    Navbar logo added and about us page implemented

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,10 +5,21 @@ function About() {
     return (
         <>
             <div className={styles.container}>
+                <div className={styles.logo}>
+                <img src="https://i.ibb.co/XD62Rsw/Black-logo-no-background.png" alt = "logo"></img>
+                </div>
                 <h1>About Us</h1>
-                <p>content</p>
+                <div className={styles.content}>
+                <p>content bla bla bla</p>
+                </div>
                 <h1>Member</h1>
-                <p>content</p>
+                <div className={styles.member}>
+                <img src="https://images.unsplash.com/photo-1626092806645-ae053131caff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt = "member"></img>
+                <img src="https://images.unsplash.com/photo-1626092806645-ae053131caff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt = "member"></img>
+                <img src="https://images.unsplash.com/photo-1626092806645-ae053131caff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt = "member"></img>
+                <img src="https://images.unsplash.com/photo-1626092806645-ae053131caff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt = "member"></img>
+                <img src="https://images.unsplash.com/photo-1626092806645-ae053131caff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt = "member"></img>
+                </div>
                 
             </div>
         </>
