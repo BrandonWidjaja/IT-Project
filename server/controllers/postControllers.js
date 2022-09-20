4e743ff0fb8f4c30d0526d27e790baf3d10aff1c
jiayao3 <wujiayao.jiayao@gmail.com>
2022-09-20 17:34:12 +1000

    new branch

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -3,7 +3,9 @@ const { Post } = require("../models/post");
 const addNewPost = async (req, res, next) => {
   try {
     // create new post from req
+    let today = getDateTime().substring(0,10);
     const newPost = await new Post(req.body);
+    newPost.dateTimePosted = today;
     await newPost.save();
 
     // send new post
@@ -18,7 +20,7 @@ const addNewPost = async (req, res, next) => {
 // get a post from their objectID
 const getPost = async (req, res, next) => {
   try {
-    // retrieve object id of user from request
+    // retrieve object id of post from request
     let postID = req.body._id;
 
     // find the user in the database
@@ -34,4 +36,107 @@ const getPost = async (req, res, next) => {
   }
 };
 
-module.exports = { addNewPost, getPost };
+const addComment = async (req, res, next) => {
+  try {
+    // retrieve object id of post from request
+    let postID = req.body._id;
+    let today = getDateTime().substring(0,10)
+    let commentedBy = req.body.displayName;
+
+    let comment = {"content": req.body.newComment, "dateTimePosted": today, "postedByID": commentedBy};
+
+    // find the post in the database
+    let exists = await Post.findOne({ _id: postID });
+    if (exists) {
+      exists.comments.push(comment);
+      exists.save();
+      return res.send(exists);
+    }
+    // post not found
+    return res.send("post does not exist");
+  } catch (e) {
+    console.error(e);
+    return res.send(e);
+  }
+}
+
+const likeComment = async (req, res, next) => {
+  try {
+    // retrieve object id of post from request
+    let postID = req.body._id;
+    let commentID = req.body.commentID;
+    let user = req.body.user;
+
+    // find the post in the database
+    let exists = await Post.findOne({ _id: postID});
+    if (exists) {
+      // find the comment that matches the ID
+      for (var comment in exists.comments){
+        var foundComment = exists.comments[comment];
+        
+        if (foundComment._id.toString() == commentID.toString()){
+          // add the user to list of users that liked the comment
+          if (exists.comments[comment].likedBy.includes(user)){
+            return res.send("Comment already liked");
+          } else {
+            foundComment.likedBy.push(user);
+          }
+          
+          
+        }
+      }
+      exists.save();
+      return res.send(exists);
+    }
+    // post not found
+    return res.send("post does not exist");
+  } catch (e) {
+    console.error(e);
+    return res.send(e);
+  }
+}
+
+
+const dislikeComment = async (req, res, next) => {
+  try {
+    // retrieve object id of post from request
+    let postID = req.body._id;
+    let commentID = req.body.commentID;
+    let user = req.body.user;
+
+    // find the post in the database
+    let exists = await Post.findOne({ _id: postID});
+    if (exists) {
+      // find the comment that matches the ID
+      for (var comment in exists.comments){
+        var foundComment = exists.comments[comment];
+        
+        if (foundComment._id.toString() == commentID.toString()){
+          // add the user to list of users that liked the comment
+          if (exists.comments[comment].dislikedBy.includes(user)){
+            return res.send("Comment already disliked");
+          } else {
+            foundComment.dislikedBy.push(user);
+          }
+          
+          
+        }
+      }
+      exists.save();
+      return res.send(exists);
+    }
+    // post not found
+    return res.send("post does not exist");
+  } catch (e) {
+    console.error(e);
+    return res.send(e);
+  }
+}
+
+
+const getDateTime = () => {
+  const today = new Date().toLocaleString("en-AU", {timeZone: "Australia/Melbourne"});
+  return today;
+}
+
+module.exports = { addNewPost, getPost, addComment, likeComment, dislikeComment };
