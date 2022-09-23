6c39ae191ebed8c2fbf0df31444133b4a3b5d2e0
bhthia1 <bthia@student.unimelb.edu.au>
2022-09-23 17:19:45 +1000

    added ratings

--- server/models/building.js
+++ server/models/building.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
+const rating = new mongoose.Schema({
+  ratedByID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  ratingValue: { type: Number },
+});
+
 const buildingSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   location: { type: String, required: true },
   //image: { type: String, required: true },
+<<<<<<< Updated upstream
+=======
+  ratings: [rating],
+>>>>>>> Stashed changes
   averageRating: { type: Number, required: true, default: 0 },
   lastUpdated: { type: String },
   tags: [String],
